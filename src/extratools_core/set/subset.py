1dc006958f0caf6d6ab42829eec3ff55af3d0086
Chuancong Gao <chuanconggao@users.noreply.github.com>
2025-04-05 13:03:27 -0700

    Add best_subset in subset

--- src/extratools_core/set/subset.py
+++ src/extratools_core/set/subset.py
@@ -1,11 +1,18 @@
 from collections.abc import Callable, Iterable
 from collections.abc import Set as AbstractSet
 
-from ..seq.subseq import enumerate_subseqs_with_gaps
+from ..seq.subseq import best_subseq_with_gaps, enumerate_subseqs_with_gaps
 
 
 def enumerate_subsets[T](a: AbstractSet[T]) -> Iterable[AbstractSet[T]]:
     return map(frozenset, enumerate_subseqs_with_gaps(a))
+
+
+def best_subset[T](
+    a: set[T],
+    score_func: Callable[[Iterable[T]], float],
+) -> set[T]:
+    return set(best_subseq_with_gaps(tuple(a), score_func))
 
 
 def set_cover[T](
