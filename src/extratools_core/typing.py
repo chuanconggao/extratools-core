c41bd8e183d9cec964257ff48426344b3e1f9047
Chuancong Gao <chuanconggao@users.noreply.github.com>
2025-04-07 05:34:00 -0700

    Improve Comparable in typing

--- src/extratools_core/typing.py
+++ src/extratools_core/typing.py
@@ -9,18 +9,18 @@ class Comparable(Protocol):  # noqa: PLW1641
     """
 
     @abstractmethod
-    def __eq__(self, other: object) -> bool:
+    def __eq__(self, other: object, /) -> bool:
         ...
 
     @abstractmethod
-    def __lt__(self, other: Self) -> bool:
+    def __lt__(self, other: Self, /) -> bool:
         ...
 
-    def __gt__(self, other: Self) -> bool:
+    def __gt__(self, other: Self, /) -> bool:
         return (not self < other) and self != other
 
-    def __le__(self, other: Self) -> bool:
+    def __le__(self, other: Self, /) -> bool:
         return self < other or self == other
 
-    def __ge__(self, other: Self) -> bool:
+    def __ge__(self, other: Self, /) -> bool:
         return (not self < other)
