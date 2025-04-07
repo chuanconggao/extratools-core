3529c497acd59c3c5516befb66c42b18d0c94af0
Chuancong Gao <chuanconggao@users.noreply.github.com>
2025-04-07 05:34:28 -0700

    Add docs for math

--- src/extratools_core/mathtools.py
+++ src/extratools_core/mathtools.py
@@ -4,10 +4,38 @@ from math import inf, log2
 
 
 def safediv(a: float, b: float) -> float:
+    """
+    Safely divide float even when by 0, by returning infinity with proper sign.
+
+    Parameters
+    ----------
+    a : float
+    b : float
+
+    Returns
+    -------
+    float
+        Result of `a / b`
+    """
+
     return inf * a if b == 0 else a / b
 
 
 def entropy[T](data: Iterable[T]) -> float:
+    """
+    Compute the entropy of data (as collection of items).
+
+    Parameters
+    ----------
+    data : float
+        Collection (as `Iterable`) of items
+
+    Returns
+    -------
+    float
+        Value of entropy
+    """
+
     counter: Counter[T] = Counter(data)
     total: int = sum(counter.values())
 
